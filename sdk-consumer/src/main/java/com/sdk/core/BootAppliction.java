173b73ae4ae766ba509047809b93619e04b76815
温东源 <wendyforlove1118@gmail.com>
2020-07-28 04:19:48 +0800

    去掉多余注解并限制没必要的文件git上传

--- sdk-consumer/src/main/java/com/sdk/core/BootAppliction.java
+++ sdk-consumer/src/main/java/com/sdk/core/BootAppliction.java
@@ -1,11 +1,9 @@
 package com.sdk.core;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo
 public class BootAppliction {
     public static void main(String[] args) {
         SpringApplication.run(BootAppliction.class, args);
