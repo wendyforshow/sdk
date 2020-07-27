0eab73e63a011974de896806912071a60c82811c
温东源 <wendyforlove1118@gmail.com>
2020-07-28 03:20:50 +0800

    去掉使用错误的注解

--- sdk-consumer/src/main/java/com/sdk/core/BootAppliction.java
+++ sdk-consumer/src/main/java/com/sdk/core/BootAppliction.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubbo(scanBasePackages = "com.sdk.core.controller")
+@EnableDubbo
 public class BootAppliction {
     public static void main(String[] args) {
         SpringApplication.run(BootAppliction.class, args);
